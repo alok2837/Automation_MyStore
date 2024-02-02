b58a694c368e443bfbcf9d2a91e80455be274ea9
ALOK SINGH <14109008alok@gmail.com>
2024-02-03 01:59:12 +0530

    Project update

--- src/test/java/com/mystore/testcases/LoginPageTest.java
+++ src/test/java/com/mystore/testcases/LoginPageTest.java
@@ -5,7 +5,7 @@ package com.mystore.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+// import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
